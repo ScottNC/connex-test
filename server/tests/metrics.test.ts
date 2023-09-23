81813581286eb8144d0095e73222d5384bb38a70
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-09-23 17:02:32 +0100

    tests metrics endpoint returns all metrics

--- server/tests/metrics.test.ts
+++ server/tests/metrics.test.ts
@@ -4,6 +4,36 @@ import { getHeader } from '../get_header';
 
 const header: string = getHeader();
 
+const allMetrics : string[] = [
+  `http_requests_total`,
+  `http_request_duration_seconds_bucket`,
+  'process_cpu_user_seconds_total',
+  'process_cpu_system_seconds_total',
+  'process_cpu_seconds_total',
+  'process_start_time_seconds',
+  'process_resident_memory_bytes',
+  'nodejs_eventloop_lag_seconds',
+  'nodejs_eventloop_lag_min_seconds',
+  'nodejs_eventloop_lag_max_seconds',
+  'nodejs_eventloop_lag_mean_seconds',
+  'nodejs_eventloop_lag_stddev_seconds',
+  'nodejs_eventloop_lag_p50_seconds',
+  'nodejs_eventloop_lag_p90_seconds',
+  'nodejs_eventloop_lag_p99_seconds',
+  'nodejs_active_handles',
+  'nodejs_active_handles_total',
+  'nodejs_active_requests',
+  'nodejs_active_requests_total',
+  'nodejs_heap_size_total_bytes',
+  'nodejs_heap_size_used_bytes',
+  'nodejs_external_memory_bytes',
+  'nodejs_heap_space_size_total_bytes',
+  'nodejs_heap_space_size_used_bytes',
+  'nodejs_heap_space_size_available_bytes',
+  'nodejs_version_info',
+  'nodejs_gc_duration_seconds',
+]
+
 describe('metrics endpoint', () => {
   afterAll(async () => {
     try {
@@ -22,4 +52,19 @@ describe('metrics endpoint', () => {
     const response = await request(app).get('/metrics').set('Authorization', header + 'abc');
     expect(response.statusCode).toBe(403);
   });
+
+  it('should return text with the correct header', async () => {
+    const response = await request(app).get('/metrics').set('Authorization', header);
+    expect(response.statusCode).toBe(200);
+    expect(response.type).toBe('text/plain');
+  });
+
+  it('should return the prometheus API metrics when after /time is called', async () => {
+    await request(app).get('/time').set('Authorization', header);
+    const response = await request(app).get('/metrics').set('Authorization', header);
+    const { text } = response;
+    expect(text).toContain('http_requests_total{route="/time",method="GET",status="2XX"} 1'); 
+    
+    allMetrics.forEach(metric => expect(text).toContain(metric));
+  })
 });
